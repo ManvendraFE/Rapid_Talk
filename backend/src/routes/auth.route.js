ba094396e3f1645d4c4a36711b1bfa44fcc7bfbd
Manvendra Gautam <manvendragautam123@gmail.com>
2026-06-18 08:36:02 +0530

    update profile endpoint added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -12,5 +12,5 @@ router.post("/logout",logout)
 
 router.put("/update-profile", protectedRoute, updateProfile); // protectedRoute middleware use karna hai, taki sirf authenticated user hi apna profile update kar sake
 
-router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+router.get("/check", protectedRoute, (req, res) => res.status(200).json(req.user));
 export default router;
