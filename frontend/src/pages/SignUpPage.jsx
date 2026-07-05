fac9a3abd3c4240c4960c337db3d5123a3e5fed2
Manvendra Gautam <manvendragautam123@gmail.com>
2026-07-05 13:20:29 +0530

    signup implemented on the frontend

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import {useAuthStore} from "../store/useAuthStore"
+import BoarderAnimatedContainer from "../components/BorderAnimatedContainer"
+import { MessageCircle, LockIcon, MailIcon, UserIcon, LoaderIcon } from "lucide-react";
+function SignUpPage(){
+  const  [formData, setFormData] = useState({fullName:"", email:"", password:""}) 
+  const {signup, isSigningUp} = useAuthStore()
 
-import { useAuthStore } from '../store/useAuthStore';
+  const handleSubmit = (e) => {} //
+  return <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+    <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]"></div>
+    <BoarderAnimatedContainer>
+      <div className="w-full flex flex-col md:flex-row">
+         {/* FORM COL --LEFT SIDE */}
+         <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30 ">
+         <div className="w-full max-w-md">
+          {/* HEADING TEXT */}
+          <div className="text-center mb-8">
+            <MessageCircleIconc className="w-12 h-12 mx-auto text-slate-400 mb-4"/>
+            <h2 className="text-2xl font-bold text-slate-200 mb-2">Create Account</h2>
+            <p className="text-slate-400">Sign uyp for a new account</p>
 
-function SignUpPage() {
-  const {authUser,isLoggedIn, login}= useAuthStore();
-  return (
-    <div>
-      SignUpPage
-    </div>
-  )
+          </div>
+          {/*FORM */}
+          <form onSubmit={handleSubmit}></form>
+         </div>
+         </div>
+
+      </div>
+     </BoarderAnimatedContainer>
+  </div>   
 }
-
-export default SignUpPage
+export default SignUpPage;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,12 +1,35 @@
 import {create} from 'zustand' ;
+import {axiosInstance} from "../lib/axios.js"
+import toast from "react-hot-toast";
+export const useAuthStore = create((set)=>({
+   authUser: null, // if user is authenticated we can set user object here
+   isCheckingAuth: true,
+   isSigningUp: false,
 
-export const useAuthStore = create((set, get)=>({
-  authUser : {name:"john", _id:123, age:25},
-  isLoggedIn:false,
-  //isLoggedIn
+   checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check")
+      set({authUser: res.data})
+    } catch (error) {
+      console.log("Error in authCheck:", error);
+      set({authUser:null})
+    }finally{
+      set({isCheckingAuth: false});
+    }
+   },
 
-  login: () => {
-    console.log("We just logged in");
-    set({isLoggedIn: true});
-  },
+   signup: async(data)=>{  // data is the formData from SignUpPage.jsx 
+    set({ isSigningUp: true})
+    try {
+     const res = await axiosInstance.post("/auth/signup", data);
+
+     set({authUser: res.data});
+
+     toast.success("Account created successfully!")
+    } catch (error) {
+      toast.error(error.response.data.message)// we can access to the error in Axios
+    }finally {
+      set({ isSigningUp: false})
+    }
+   } 
 })) 
